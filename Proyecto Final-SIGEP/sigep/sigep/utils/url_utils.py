4d6ff66a7bfc7d8b66aaa94f660feaa28880a1e3
sebastianvalbuena <46907467+sebastianvalbuena@users.noreply.github.com>
2019-05-22 10:16:55 -0500

    Final

--- Proyecto Final-SIGEP/sigep/sigep/utils/url_utils.py
+++
@@ -1,7 +0,0 @@
-base_url = 'https://www.funcionpublica.gov.co/dafpIndexerBHV/?find=FindNext&entidadSeleccionado=%s&max=%s'
-
-
-class UrlUtils:
-    @staticmethod
-    def get_url_by_entity(code, max=100):
-        return base_url % (code, max)
